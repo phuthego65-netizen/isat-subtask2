12bd7f7b6276a9f756a9e79233df68d432d6e1d8
KGOPANE MP <phuthego65@gmail.com>
2026-08-02 22:38:03 +0200

    C++ program and menu

---
+++ Subtask2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int choice, num, decimal = 0;
+    string binary;
+
+    cout << "1. Decimal to Binary" << endl;
+    cout << "2. Binary to Decimal" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        cout << "Enter a decimal number: ";
+        cin >> num;
+
+        while (num > 0)
+        {
+            binary = char((num % 2) + '0') + binary;
+            num = num / 2;
+        }
+
+        cout << "Binary = " << binary << endl;
+    }
+    else if (choice == 2)
+    {
+        cout << "Enter a binary number: ";
+        cin >> binary;
+
+        for (int i = 0; i < binary.length(); i++)
+        {
+            decimal = decimal * 2 + (binary[i] - '0');
+        }
+
+        cout << "Decimal = " << decimal << endl;
+    }
+    else
+    {
+        cout << "Invalid choice!";
+    }
+
+    return 0;
+}
